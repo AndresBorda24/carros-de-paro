2d50809be90d659796858cd459ffd4c0377117b2
Andres Borda <ajborda4@misena.edu.co>
2023-06-05 11:40:18 -0500

    Medoo Framework para base de datos

--- config/ContainerBindings.php
+++ config/ContainerBindings.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 use App\Config;
+use Medoo\Medoo;
 use DI\ContainerBuilder;
 
 $container = new ContainerBuilder();
@@ -10,6 +11,16 @@ $container->addDefinitions([
     Config::class => function () {
         return new Config(require __DIR__ . "/configs.php");
     },
+    Medoo::class => function (Config $c) {
+        return new Medoo([
+            'type' => 'mysql',
+            'host' => $c->get('db.host'),
+            'database' => $c->get('db.name'),
+            'username' => $c->get('db.user'),
+            'password' => $c->get('db.pass'),
+            'port' => $c->get('db.port', 3306),
+        ]);
+    }
 ]);
 
 return $container->build();
