a09b08632f3429f6f91758bf1cffb1bafd6bc6df
Andres Borda <ajborda4@misena.edu.co>
2023-06-07 17:26:42 -0500

    Ayuda para identificar los colores

--- templates/partials/carro/carro.php
+++ templates/partials/carro/carro.php
@@ -1,9 +1,13 @@
-<div x-data="carro" x-bind="events" class="p-3 p-md-4">
+<div x-data="carro" x-bind="events" class="p-3 p-md-4 mb-4">
+
+  <!-- Titulo y Boton de Eliminar -->
   <div class="d-flex align-items-center flex-wrap border-bottom p-1 mb-3">
     <div class="flex-grow-1 gap-1 text-center d-flex justify-content-center align-items-center">
       <h5
       class="m-0"
       x-text="getCarroNombre() || 'Selecciona un Carro'"></h5>
+
+      <!-- Boton de Editar Carro -->
       <span
       role="button"
       @click="$dispatch('update-carro', getCarroFull())"
@@ -13,6 +17,8 @@
         <?= $this->fetch("./icons/edit.php") ?>
       </span>
     </div>
+
+    <!-- Eliminar Carro -->
     <button
     type="button"
     @click="delCarro"
@@ -25,22 +31,52 @@
     </button>
   </div>
 
-  <template x-if="hasCarro">
-    <div class="btn-group mb-3" role="group">
-      <button
-      type="button"
-      @click="grillaShow = 1"
-      :class="{'active': grillaShow === 1}"
-      class="btn btn-outline-primary btn-sm text-sm active">Medicamentos</button>
-      <button
-      type="button"
-      @click="grillaShow = 2"
-      :class="{'active': grillaShow === 2}"
-      class="btn btn-outline-primary btn-sm text-sm">Dispositivos</button>
-    </div>
-  </template>
 
+  <!-- 'Nav' (medicamentos-dispositivos) y detalles de colores -->
+  <div class="d-flex flex-wrap mb-3 justify-content-between">
+    <!-- Nav -->
+    <template x-if="hasCarro">
+      <div class="btn-group" role="group">
+        <button
+        type="button"
+        @click="grillaShow = 1"
+        :class="{'active': grillaShow === 1}"
+        class="btn btn-outline-primary btn-sm text-sm active">Medicamentos</button>
+        <button
+        type="button"
+        @click="grillaShow = 2"
+        :class="{'active': grillaShow === 2}"
+        class="btn btn-outline-primary btn-sm text-sm">Dispositivos</button>
+      </div>
+    </template>
 
+    <details
+    x-cloak
+    x-show="hasCarro"
+    class="position-relative">
+      <summary class="text-sm btn">
+          &iquest;Ayuda con los colores?
+          <?= $this->fetch("./icons/question.php") ?>
+      </summary>
+
+      <ul
+      style="width: 210px; z-index: 1;"
+      class="list-group list-group-flush position-absolute text-sm end-0
+      top-100 shadow border border-secondary rounded-1 mt-1">
+        <li class="list-group-item list-group-item-success">
+          M&aacute;s a 12 Meses
+        </li>
+        <li class="list-group-item list-group-item-warning">
+          Entre 7 y 12 Meses
+        </li>
+        <li class="list-group-item list-group-item-danger">
+          Menos de 7 meses
+        </li>
+      </ul>
+    </details>
+  </div>
+
+  <!-- Grillas -->
   <div x-show="grillaShow === 1">
     <?= $this->fetch("./partials/carro/grillas/medicamentos.php") ?>
   </div>
