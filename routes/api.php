29a175f42ee6f228b795fe1e78f52b441a77747c
AndresBorda24 <anjart24@gmail.com>
2023-08-03 17:21:17 -0500

    Ajustes rutas API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 declare(strict_types=1);
 
-use App\Controllers\Api\AperturasController;
 use Slim\App;
+use App\Controllers\Api\AperturasController;
 use App\Controllers\Api\CarroController;
 use App\Controllers\Api\DispositivoController;
 use App\Controllers\Api\HistoricoController;
@@ -53,11 +53,11 @@ function loadApiRoutes(App $app) {
         /* ---------------------------------------------------------------------
         *  Medicamentos
         */
-        $api->post("/medicamentos/create", [
+        $api->post("/medicamentos/create/{apId:[0-9]+}", [
             MedicamentoController::class,
             "create"
         ]);
-        $api->put("/medicamentos/{id:[0-9]+}/update", [
+        $api->put("/medicamentos/{id:[0-9]+}/update/{apId:[0-9]+}", [
             MedicamentoController::class,
             "update"
         ]);
@@ -65,7 +65,7 @@ function loadApiRoutes(App $app) {
             MedicamentoController::class,
             "updateCarro"
         ]);
-        $api->delete("/medicamentos/{id:[0-9]+}/delete", [
+        $api->delete("/medicamentos/{id:[0-9]+}/delete/{apId:[0-9]+}", [
             MedicamentoController::class,
             "delete"
         ]);
@@ -73,11 +73,11 @@ function loadApiRoutes(App $app) {
         /* ---------------------------------------------------------------------
         *  Dispositivos
         */
-        $api->post("/dispositivos/create", [
+        $api->post("/dispositivos/create/{apId:[0-9]+}", [
             DispositivoController::class,
             "create"
         ]);
-        $api->put("/dispositivos/{id:[0-9]+}/update", [
+        $api->put("/dispositivos/{id:[0-9]+}/update/{apId:[0-9]+}", [
             DispositivoController::class,
             "update"
         ]);
@@ -85,7 +85,7 @@ function loadApiRoutes(App $app) {
             DispositivoController::class,
             "updateCarro"
         ]);
-        $api->delete("/dispositivos/{id:[0-9]+}/delete", [
+        $api->delete("/dispositivos/{id:[0-9]+}/delete/{apId:[0-9]+}", [
             DispositivoController::class,
             "delete"
         ]);
