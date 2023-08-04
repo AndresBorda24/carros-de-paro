e2fa316afe0a2318ee4479e8b4e46e7ce5e81294
AndresBorda24 <anjart24@gmail.com>
2023-08-04 11:58:23 -0500

    Request -> BodyRequest

--- app/Requests/MedicamentosRequest.php
+++ app/Requests/MedicamentosRequest.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace App\Requests;
 
-class MedicamentoRequest extends BodyRequest
+class MedicamentosRequest extends BodyRequest
 {
     /**
      * Valida que los datos de la solicitud sean correctos. Helper con
